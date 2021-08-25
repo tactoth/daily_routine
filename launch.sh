4123856c1338076b8ab088d2f7a664a8d9ec12dc
Wei Liu <wei.liu@nobody.com>
2021-08-24 22:51:52 -0700

    better lnf

--- launch.sh
+++ launch.sh
@@ -1,1 +1,2 @@
-java -jar target/scala-2.11/daily_routine-assembly-1.0.jar > /dev/null 2>&1 &
+# https://stackoverflow.com/a/56487943
+java -jar target/scala-2.11/daily_routine-assembly-1.0.jar -NSRequiresAquaSystemAppearance False > /dev/null 2>&1 &
